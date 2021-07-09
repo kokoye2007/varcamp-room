ac6cab4cbb354c1fd657afd79428ed7d2572f494
Valdo Romao <v.romao@thecodingmachine.com>
2021-07-09 23:41:40 +0100

    Popups follow and help

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,66 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 
-// You can write your WorkAdventure script here, if any.
-// The "WA" global object is available from anywhere.
 
 console.log('Script started successfully');
-WA.openCoWebSite('https://workadventu.re');
+
+let currentZone: string;
+let currentPopup: any;
+
+const config = [
+    {
+        zone: 'needHelp',
+        message: 'Do you need some guidance? We are happy to help you out.',
+        cta: [
+            {
+                label: 'Meet us',
+                className: 'primary',
+                callback: () => WA.openTab('https://play.staging.workadventu.re/@/tcm/workadventure/wa-village'),
+            }
+        ]
+    },
+    {
+        zone: 'followUs',
+        message: 'Hey! Have you already started following us?',
+        cta: [
+            {
+                label: 'LinkedIn',
+                className: 'primary',
+                callback: () => WA.openTab('https://www.linkedin.com/company/workadventu-re'),
+            },
+            {
+                label: 'Twitter',
+                className: 'primary',
+                callback: () => WA.openTab('https://twitter.com/workadventure_'),
+            }
+        ]
+    },
+]
+
+
+WA.onEnterZone('needHelp', () => {
+    currentZone = 'needHelp'
+    openPopup(currentZone, currentZone + 'Popup')
+});
+WA.onEnterZone('followUs', () => {
+    currentZone = 'followUs'
+    openPopup(currentZone, currentZone + 'Popup')
+});
+WA.onLeaveZone('needHelp', closePopup);
+WA.onLeaveZone('followUs', closePopup);
+
+
+function openPopup(zoneName: string, popupName: string) {
+    const zone = config.find((item) => {
+        return item.zone == zoneName
+    });
+    if (typeof zone !== 'undefined') {
+        // @ts-ignore otherwise we can't use zone.cta object
+        currentPopup = WA.openPopup(popupName, zone.message, zone.cta)
+    }
+}
+function closePopup(){
+    if (typeof currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
